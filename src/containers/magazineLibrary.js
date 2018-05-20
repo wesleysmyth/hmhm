be89dcce90a1f04b8d87f85210a4d8b0051e2c9a
Wesley Smith <wesleytatesmith@gmail.com>
2018-05-20 15:26:48 -0400

    Add boilerplate container code

--- src/containers/magazineLibrary.js
+++ src/containers/magazineLibrary.js
@@ -10,7 +10,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        // fetchVideos: fetchVideos()
+        // fetchMagazines: dispatch(fetchMagazines()),
     };
 }
 
